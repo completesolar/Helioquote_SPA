65b883e132ab79804d1d15535779c7fecd055ef8
Alex Moralez <alex10klymenko@gmail.com>
2020-08-17 17:51:58 +0300

    added mat dialog to feedback circle

--- src/app/shared/components/feedback-circle/feedback-circle.component.ts
+++ src/app/shared/components/feedback-circle/feedback-circle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { SupportDialogComponent } from '../support-dialog/support-dialog.component';
 
 @Component({
   selector: 'app-feedback-circle',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackCircleComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
